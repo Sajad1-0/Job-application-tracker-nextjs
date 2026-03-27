753d45819163d7e3a2fc925788b7a38789279e92
Sajjad <sajjadqaderi00@gmail.com>
2026-03-27 14:00:59 +0100

    setting up MongoDB envinronment, not done yet

---
+++ lib/db.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error('Please define the MongoDB environment variable inside .env');
+}
+
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
+declare global {
+  var mongoose: MongooseCache | undefined;
+}
+
+// eslint-disable-next-line prefer-const
+let cached: MongooseCache = global.mongoose || { conn: null, promise: null };
+
+if (!global.mongoose) {
+  global.mongoose = cached;
+}
