198db885269762fe3ea903ca830c7515d52c0576
Sajjad <sajjadqaderi00@gmail.com>
2026-04-02 15:02:42 +0200

    WIP dashboard page

---
+++ components/kanban-board.tsx
@@ -0,0 +1,14 @@
+'use client';
+import { Board } from '@/lib/models/models.types';
+import React from 'react';
+
+interface KanbanBoardProps {
+  board: Board;
+  userId: string;
+}
+
+const KanbanBoard = ({ board, userId }: KanbanBoardProps) => {
+  return <div>KanbanBoard</div>;
+};
+
+export default KanbanBoard;
